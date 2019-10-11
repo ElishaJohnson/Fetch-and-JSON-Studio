bdb6010da9b7d22772b5945a1986b7c9e46cd621
Elisha Johnson <elishamagicjohnson@gmail.com>
2019-10-10 19:18:04 -0500

    finished studio

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-// TODO: add code here
+window.addEventListener("load", function () {
+    let container = document.getElementById("container");
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function (response) {
+        response.json().then(function (json) {
+            container.innerHTML = `Number of astronauts: ${json.length}<br><br>`;
+            json.sort((a, b) => (a.hoursInSpace > b.hoursInSpace) ? -1 : 1)
+            for (let i = 0; i < json.length; i++) {
+                container.innerHTML += `
+                    <div class="astronaut">
+                        <div class="bio">
+                            <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                            <ul>
+                                <li>Hours in space: ${json[i].hoursInSpace}</li>
+                                <li id=activeLine${i}>Active: ${json[i].active}</li>
+                                <li>Skills: ${json[i].skills}</li>
+                            </ul>
+                        </div>
+                        <img class="avatar" src="${json[i].picture}">
+                    </div>
+                `;
+                if (json[i].active) {
+                    document.getElementById("activeLine" + i).style.color = "green";
+                }
+            }
+        });
+    });
+});
